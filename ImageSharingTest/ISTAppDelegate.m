a66d67f71446139dfd9caa796daae3adfa128dd0
Gleb Dolgich <glebd@me.com>
2013-01-26 17:20:19 +0000

    Implement image selection

--- ImageSharingTest/ISTAppDelegate.m
+++ ImageSharingTest/ISTAppDelegate.m
@@ -14,7 +14,18 @@
 }
 
 - (IBAction)chooseImage:(id)sender {
-
+	NSOpenPanel *openPanel = [[NSOpenPanel alloc] init];
+	[openPanel setCanChooseFiles:YES];
+	[openPanel setCanChooseDirectories:NO];
+	[openPanel setCanCreateDirectories:NO];
+	[openPanel setAllowsMultipleSelection:NO];
+	[openPanel setAllowedFileTypes:@[@"jpg", @"jpeg", @"gif", @"png", @"tiff", @"tif"]];
+	[openPanel beginSheetModalForWindow:self.window completionHandler:^(NSInteger result) {
+		if (result == NSOKButton) {
+			NSURL *url = [openPanel URLs][0];
+			self.imagePath = [url path];
+		}
+	}];
 }
 
 - (IBAction)shareUsingTempFile:(id)sender {
