c703406c6ae180d39b174188286e370514a8c059
Gleb Dolgich <glebd70@gmail.com>
2013-01-27 22:21:17 +0000

    Add image wrapper class

---
+++ ImageSharingTest/ISTImage.h
@@ -0,0 +1,13 @@
+//
+//  ISTImage.h
+//  ImageSharingTest
+//
+//  Created by Gleb on 27/01/2013.
+//  Copyright (c) 2013 PixelEspresso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ISTImage : NSObject
+
+@end
---
+++ ImageSharingTest/ISTImage.m
@@ -0,0 +1,13 @@
+//
+//  ISTImage.m
+//  ImageSharingTest
+//
+//  Created by Gleb on 27/01/2013.
+//  Copyright (c) 2013 PixelEspresso. All rights reserved.
+//
+
+#import "ISTImage.h"
+
+@implementation ISTImage
+
+@end
